7723d90394151abec80f5c93ad278780d0be7730
Alberto <arodriguezg@iesch.org>
2020-09-30 19:59:44 +0200

    Aplicacion terminada

--- app/src/main/java/org/iesch/alberto/a3_registrodesocios/DetailActivity.java
+++ app/src/main/java/org/iesch/alberto/a3_registrodesocios/DetailActivity.java
@@ -17,15 +17,23 @@ public class DetailActivity extends AppCompatActivity {
         //Este getExtras va a contener todos valores
         Bundle extras = getIntent().getExtras();
 
+        // 8 - Recibimos el objeto Socio
+        Socio nuevoSocio = extras.getParcelable("nuevoSocio");
+        /* Ya no necesitamos nada de esto
         String name = extras.getString("name");
         String last_name = extras.getString("last_name");
         String nick = extras.getString("nick");
         String edad = extras.getString("edad");
         String direccion = extras.getString("direccion");
         String ciudad = extras.getString("ciudad");
+        */
 
         //Mostramos por pantalla todos los valores que hemos recibido
-        respuesta.setText(name + " ," + last_name + " ," + nick + " ," + edad + " ," + direccion + " ," + ciudad);
+        //respuesta.setText(name + " ," + last_name + " ," + nick + " ," + edad + " ," + direccion + " ," + ciudad);
+        //Punto 8
+        if (nuevoSocio != null) {
+            respuesta.setText(nuevoSocio.getName() + " ," + nuevoSocio.getLastName() + " ," + nuevoSocio.getNickName() + " ," + nuevoSocio.getEdad() + " ," + nuevoSocio.getDireccion() + " ," + nuevoSocio.getCiudad());
+        }
 
 
     }
